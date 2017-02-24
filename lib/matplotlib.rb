966602df24ac388c55df12e7b8b55c310447286c
Kenta Murata <mrkn@mrkn.jp>
2017-02-24 17:43:03 +0900

    Add Matplotlib::Pyplot module

--- lib/matplotlib.rb
+++ lib/matplotlib.rb
@@ -1,5 +1,28 @@
 require "matplotlib/version"
+require 'pycall/import'
 
 module Matplotlib
-  # Your code goes here...
+  @matplotlib = PyCall.import_module('matplotlib')
+  PyCall.dir(@matplotlib).each do |name|
+    obj = PyCall.getattr(@matplotlib, name)
+    next unless obj.kind_of? PyCall::PyObject
+    next unless obj.kind_of? PyCall::LibPython.PyFunction_Type
+
+    define_singleton_method(name) do |*args, **kwargs|
+      obj.(*args, **kwargs)
+    end
+  end
+
+  class << self
+    def method_missing(name, *args, **kwargs)
+      return super unless PyCall.hasattr?(@matplotlib, name)
+      PyCall.getattr(@matplotlib, name)
+    end
+  end
+
+  # FIXME: MacOSX backend is unavailable via pycall.
+  #        I don't know why it is.
+  if Matplotlib.get_backend() == 'MacOSX'
+    Matplotlib.use('TkAgg')
+  end
 end
--- lib/matplotlib/pyplot.rb
+++ lib/matplotlib/pyplot.rb
@@ -1,17 +1,22 @@
-require 'pycall/import'
+require 'matplotlib'
 
 module Matplotlib
-  module PyPlot
-    dir = PyCall.eval('dir')
+  module Pyplot
     @pyplot = PyCall.import_module('matplotlib.pyplot')
-
-    dir.(@pyplot).each do |name|
-      obj = @pyplot.__send__(name)
+    PyCall.dir(@pyplot).each do |name|
+      obj = PyCall.getattr(@pyplot, name)
       next unless obj.kind_of? PyCall::PyObject
-      next unless obj.__class__.__name__ == 'function'
+      next unless obj.kind_of? PyCall::LibPython.PyFunction_Type
 
       define_singleton_method(name) do |*args, **kwargs|
         obj.(*args, **kwargs)
+      end
+    end
+
+    class << self
+      def method_missing(name, *args, **kwargs)
+        return super unless PyCall.hasattr?(@pyplot, name)
+        PyCall.getattr(@pyplot, name)
       end
     end
   end
