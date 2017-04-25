4b854b31ef7d5eaed6c64ae0a0645eb092d543c9
Kenta Murata <mrkn@mrkn.jp>
2017-04-25 10:27:18 -0700

    Remove meaningless example

--- spec/matplotlib_spec.rb
+++ spec/matplotlib_spec.rb
@@ -4,8 +4,4 @@ describe Matplotlib do
   it "has a version number" do
     expect(Matplotlib::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
