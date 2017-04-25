cbc84fc9e46a0b30dc31793eeb892b8764ad04b9
Kenta Murata <mrkn@mrkn.jp>
2017-04-25 10:30:49 -0700

    Combine examples

--- spec/matplotlib/pyplot_spec.rb
+++ spec/matplotlib/pyplot_spec.rb
@@ -5,16 +5,15 @@ module Matplotlib
   ::RSpec.describe Pyplot do
     describe '.xkcd' do
       specify do
-        expect { |b| Pyplot.xkcd(&b) }.to yield_control
-      end
-
-      specify do
         saved_font_family = Matplotlib.rcParams['font.family']
         saved_path_sketch = Matplotlib.rcParams['path.sketch']
-        Pyplot.xkcd(scale: 42, length: 43, randomness: 44) do
-          expect(Matplotlib.rcParams['font.family']).to include('xkcd')
-          expect(Matplotlib.rcParams['path.sketch']).to eq(PyCall.tuple(42, 43, 44))
-        end
+        expect { |b|
+          Pyplot.xkcd(scale: 42, length: 43, randomness: 44) do
+            lambda(&b).call
+            expect(Matplotlib.rcParams['font.family']).to include('xkcd')
+            expect(Matplotlib.rcParams['path.sketch']).to eq(PyCall.tuple(42, 43, 44))
+          end
+        }.to yield_control
         expect(Matplotlib.rcParams['font.family']).to eq(saved_font_family)
         expect(Matplotlib.rcParams['path.sketch']).to eq(saved_path_sketch)
       end
